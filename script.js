e3334de0d7f77cc731b4c1fb304f900cd9b0490a
Glitch (hello-webpage) <none>
2020-05-05 09:53:28 +0000

    🐓⛈ Checkpoint ./script.js:11290180/792

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ class dict {
     return this.key.length
   }
   add(a, b){
-    if (b!=null&&b!=null&& this.key.indexOf(a) ==-1){
-    this.key+=a;
-    this.value+=b}
+    if (b!=null)
+      if(b!=null)if this.key.indexOf(a) ==-1){
+    this.key.push(a);
+    this.value.push(b)}
+    
     
   }
   
   getByKey(a){
-    t
+    this.pos = this.key.indexOf(a);
+    return this.value[this.pos]
+    
   }
 }
 
+var dictionary = new dict();
+dictionary.add("hi", "tschüss");
+dictionary.add("hi","bye")
+console.log(dictionary.getByKey("hi"))
